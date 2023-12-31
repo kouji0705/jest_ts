e40c30e18dd7319e6d50fee29bdc9faa2ee5edcb
koji_kawamura <45913424+kouji0705@users.noreply.github.com>
2023-12-31 17:53:28 +0900

    api test (#2)
    * add: api test
    
    * wip:
    
    * chore: 不要なコード削除

---
+++ src/__tests__/api.test.ts
@@ -0,0 +1,25 @@
+import axios from 'axios';
+import { fetchData } from '@/api';
+
+// axios.get を手動でモックに設定
+axios.get = jest.fn();
+
+describe('fetchData', () => {
+  it('fetches successfully data from an API', async () => {
+    // mockを準備
+    const data = { data: 'some data' };
+    (axios.get as jest.Mock).mockResolvedValue({ data });
+
+    await expect(fetchData('https://example.com')).resolves.toEqual(data);
+  });
+
+  it('fetches erroneously data from an API', async () => {
+    // mockを準備
+    const errorMessage = 'Network Error';
+    (axios.get as jest.Mock).mockRejectedValue(new Error(errorMessage));
+
+    await expect(fetchData('https://example.com')).rejects.toThrow(
+      errorMessage
+    );
+  });
+});
